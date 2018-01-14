1514ea793db323956abd71faac6b18a9c9cdac4b
dynekpl <dynek82@gmail.com>
2018-01-14 12:07:48 +0100

    Observer and subscribe (example 2 with JSON source)

--- main.ts
+++ main.ts
@@ -1,28 +1,49 @@
 import {Observable} from "rxjs";
 
-let numbers = [1, 5, 5, 6, 7, 8, 9, 10];
-let source = Observable.create(observer => {
+const button = document.querySelector('button');
+const output = document.querySelector('#output');
 
-    let index = 0;
-    let produceValue = () => {
-        observer.next(numbers[index++]);
+let click = Observable.fromEvent(button, 'click');
 
-        if (index < numbers.length) {
-            //zaimplementowana asynchroniczność
-            setTimeout(produceValue, 1250);
-        }
-        else {
-            observer.complete();
-        }
-    }
+function load(url: string) {
 
-    produceValue();
+    return Observable.create(observer => {
+        let xhr = new XMLHttpRequest();
 
-}).map(e => e * 3).map(e => e > 18 ? `pełnoletni ${e} lat` : `niepełnoletni ${e} lat`);
+        xhr.addEventListener("load", () => {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+                //console.table(data);
+            }
+            else {
+                //zawsze kiedy połączenia z serwerem to trzeba obsłużyć błędy na wypadek problemó z transmisją danych
+                observer.error(xhr.statusText);
+            }
+        })
+        xhr.open("GET", url);
+        xhr.send();
+    }).map((e) => e.sort((a,b) => a.pages - b.pages));
+}
 
+function renderBooks(books) {
+    //sortBooks(books);
+    books.forEach(b => {
+        let node = document.createElement('div');
+        node.innerText = b.title + ", liczba stron: " + b.pages;
+        output.appendChild(node);
+    })
+}
 
-source.subscribe(
-    value => console.log(`value: ${value}`),
-    e => console.log(`error: ${e}`),
-    () => console.log("complete")
-);
+//function sortBooks(books){
+//    books.sort((e1,e2) => e2.pages - e1.pages);
+//}
+
+//poniższa linijka łączy dwa streamy z różnych obserwatorów
+click.flatMap(e => load("/books-api.json"))
+    .subscribe(
+        (e) => renderBooks(e),
+        (e) => console.log(`error ${e}`),
+        () => console.log('Done')
+    )
